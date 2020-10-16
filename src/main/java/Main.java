194018cd63acab9b2747bdc6fef31b8983a93c31
MaratArkhipov <maratkzn2010@yandex.ru>
2020-10-16 18:13:00 +0400

    all done

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Function<String, List<String>> dictionary = x -> Arrays.stream(x.split(" ")).sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+        dictionary.apply("a d c e b").forEach(System.out::println);
+        // Признаки функционального программирования:
+        // 1. Функциональный интерфейс + лямда
+        // 2. В качетсве монады Stream API
+        // 3. Ссылка на метод
+    }
+}
