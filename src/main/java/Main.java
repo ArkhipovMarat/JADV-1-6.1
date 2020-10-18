4a829f53dc93d54c19b37f7fca628005e4a7d0ed
MaratArkhipov <maratkzn2010@yandex.ru>
2020-10-18 15:55:56 +0400

    .distinct added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,10 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        Function<String, List<String>> dictionary = x -> Arrays.stream(x.split(" ")).sorted(Comparator.naturalOrder()).collect(Collectors.toList());
-        dictionary.apply("a d c e b").forEach(System.out::println);
+        Function<String, List<String>> dictionary = x -> Arrays.stream(x.split(" ")).
+                distinct().sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+        dictionary.apply("a d c e b a").forEach(System.out::println);
+        
         // Признаки функционального программирования:
         // 1. Функциональный интерфейс + лямда
         // 2. В качетсве монады Stream API
